2f2430b2d2c835e89bc633afc59e962488ec64bd
papautka <120500uas@gmail.com>
2024-03-28 14:16:52 +0300

    connect_navigation

--- app/src/main/java/com/uteev/convertoruteev/screens/RootFragment.kt
+++ app/src/main/java/com/uteev/convertoruteev/screens/RootFragment.kt
@@ -1,21 +1,39 @@
 package com.uteev.convertoruteev.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.uteev.convertoruteev.R
+import com.uteev.convertoruteev.ViewPagerAdapter
 
 
 class RootFragment : Fragment() {
 
+    private var ctx : Context? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        ctx = context
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_root, container, false)
+        val view = inflater.inflate(R.layout.fragment_root, container, false)
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
+
+        viewPager.adapter = ViewPagerAdapter(ctx as FragmentActivity)
+
+        return view
     }
 
 }
