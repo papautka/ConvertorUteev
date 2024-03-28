c68b4273a38d71a9bcea54462038efdfa08a4988
papautka <120500uas@gmail.com>
2024-03-28 11:51:19 +0300

    create_sample_start_adapter

--- app/src/main/java/com/uteev/convertoruteev/screens/start/StartAdapter.kt
+++ app/src/main/java/com/uteev/convertoruteev/screens/start/StartAdapter.kt
@@ -1,4 +1,25 @@
 package com.uteev.convertoruteev.screens.start
 
-class StartAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class StartAdapter : RecyclerView.Adapter<StartAdapter.StartViewHolder>() {
+
+    class StartViewHolder(view : View) : RecyclerView.ViewHolder (view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StartViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: StartViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
