f74a11011b3b094a55c156e0122c993f990bcfa9
papautka <120500uas@gmail.com>
2024-03-28 18:17:44 +0300

    finish_project

--- app/src/main/java/com/uteev/convertoruteev/screens/second/SecondFragment.kt
+++ app/src/main/java/com/uteev/convertoruteev/screens/second/SecondFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.uteev.convertoruteev.R
+import com.uteev.convertoruteev.screens.start.StartAdapter
+import com.uteev.convertoruteev.screens.start.StartViewModel
+import org.w3c.dom.Text
 
 class SecondFragment : Fragment() {
+
+    private lateinit var textView: TextView
+    private val viewModel: SecondViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val viewSecond = inflater.inflate(R.layout.fragment_second, container, false)
+        textView = viewSecond.findViewById(R.id.text_second)
+        viewModel.getNalMoney()
+        viewModel.myMoneyList.observe(viewLifecycleOwner) { response ->
+               response.body()?.let { currencyResponse ->
+                   textView.text = currencyResponse.Date ?: "Нет данных"
+               }
+        }
+        return viewSecond
     }
+
+
 }
--- app/src/main/java/com/uteev/convertoruteev/screens/second/SecondViewModel.kt
+++ app/src/main/java/com/uteev/convertoruteev/screens/second/SecondViewModel.kt
@@ -1,4 +1,21 @@
 package com.uteev.convertoruteev.screens.second
 
-class SecondViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.uteev.convertoruteev.data.repository.Repository
+import com.uteev.convertoruteev.model.cash.CurrencyResponse
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class SecondViewModel : ViewModel() {
+
+    var repo = Repository()
+    val myMoneyList : MutableLiveData<Response<CurrencyResponse>> = MutableLiveData()
+
+    fun getNalMoney() {
+        viewModelScope.launch {
+            myMoneyList.value = repo.getNal()
+        }
+    }
 }
